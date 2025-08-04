7a491e841af26cbb1d88142f2586def350969eab
Ashokkumar A <ashokkumar.a@signatech.com>
2025-08-04 08:29:24 +0000

    I want to deploy the application on Firebase with the name "Tally". If t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: 'Utsav Hisab',
+  title: 'Tally',
   description: 'Manage festival contributions and expenses',
 };
 
